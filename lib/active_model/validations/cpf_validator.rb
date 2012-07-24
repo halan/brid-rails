03e41c9ddeb147e015eb1b5c8cfed0a2e0bf20b3
Halan Pinheiro <halan.pinheiro@gmail.com>
2012-07-24 12:04:53 -0300

    Added CPF validator

---
+++ lib/active_model/validations/cpf_validator.rb
@@ -0,0 +1,16 @@
+require 'active_model/validations'
+
+module ActiveModel
+  module Validations
+    class CpfValidator < LuhnValidator
+      def validate_each(record, attr_name, value)
+        return if options[:allow_nil] && value.nil?
+
+        
+        unless(CPF.new(value).valid?() rescue false)
+          record.errors.add(attr_name, (options[:message] || 'cpf_not_valid'))
+        end
+      end
+    end
+  end
+end
--- lib/brid-rails.rb
+++ lib/brid-rails.rb
@@ -2,4 +2,5 @@ require "brid-rails/version"
 require 'active_model/validations/luhn_validator'
 require 'active_model/validations/mod10_validator'
 require 'active_model/validations/mod11_validator'
+require 'active_model/validations/cpf_validator'
 
