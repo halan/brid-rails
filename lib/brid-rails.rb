c2f97b3ec7188374c36ab2e98073afdf454f9b3f
Halan Pinheiro <halan.pinheiro@gmail.com>
2012-07-24 12:28:34 -0300

    Added requires of new validators

--- lib/brid-rails.rb
+++ lib/brid-rails.rb
@@ -5,4 +5,8 @@ require 'active_model/validations/luhn_validator'
 require 'active_model/validations/mod10_validator'
 require 'active_model/validations/mod11_validator'
 require 'active_model/validations/cpf_validator'
+require 'active_model/validations/cnpj_validator'
+require 'active_model/validations/cpf_or_cnpj_validator'
+require 'active_model/validations/pis_validator'
+require 'active_model/validations/titulo_eleitor_validator'
 
